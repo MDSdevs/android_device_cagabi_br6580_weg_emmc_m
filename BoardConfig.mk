2d795c82cb523ab261fe6d0901342d17b8b3f102
GMCadiom <gm.cadiom@gmail.com>
2017-04-16 18:57:23 +0530

    Revert "Source : UpDate The Whole Source Thx @rock12"
    This reverts commit e1183c1c47fdd233d3eec58ab80d987097e90ac6.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,8 +1,7 @@
-# mt6580 platform boardconfig
-LOCAL_PATH := device/infinix/x510
--include vendor/infinix/x510/BoardConfigVendor.mk
+# inherit from the proprietary version
+-include vendor/mediatek/mt6580/BoardConfigVendor.mk
 
-TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
+LOCAL_PATH := device/mediatek/mt6580
 
 USE_CAMERA_STUB := true
 TARGET_PROVIDES_INIT_RC := true
@@ -14,8 +13,6 @@ TARGET_NO_BOOTLOADER := true
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_NO_FACTORYIMAGE := true
 
-TARGET_LDPRELOAD += libxlog.so
-
 # CPU
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
@@ -24,7 +21,22 @@ TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
 TARGET_CPU_VARIANT := cortex-a7
 
+# Board **
+ARCH_ARM_HAVE_VFP := true
+ARCH_ARM_HAVE_NEON := true
+TARGET_ARCH_VARIANT_CPU := cortex-a7
+
+# Enable dex-preoptimization
+WITH_DEXPREOPT := false
+DONT_DEXPREOPT_PREBUILTS := true
+
+# Link against libxlog
+TARGET_LDPRELOAD += libxlog.so
+
+# Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := mt6580
+
+TARGET_USERIMAGES_SPARSE_EXT_DISABLED := false
 
 # MTK Hardware
 BOARD_HAS_MTK_HARDWARE := true
@@ -34,8 +46,24 @@ COMMON_GLOBAL_CFLAGS += -DMTK_HARDWARE -DADD_LEGACY_ACQUIRE_BUFFER_SYMBOL -DDISA
 COMMON_GLOBAL_CPPFLAGS += -DMTK_HARDWARE
 
 BOARD_CONNECTIVITY_VENDOR := MediaTek
+BOARD_CONNECTIVITY_MODULE := conn_soc
 BOARD_USES_MTK_AUDIO := true
-BOARD_USES_MTK_HARDWARE :=true
+
+# Partitions & Image
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2634022912
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 12538347520
+BOARD_CACHEIMAGE_PARTITION_SIZE := 553648128
+BOARD_CACHEIMAGE_TYPE := ext4
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+# Flags
+TARGET_GLOBAL_CFLAGS   += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
+COMMON_GLOBAL_CFLAGS += -DDISABLE_HW_ID_MATCH_CHECK
+TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
 
 # Kernel
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32S1,32S1 androidboot.selinux=permissive
@@ -45,36 +73,32 @@ BOARD_MKBOOTIMG_ARGS := --base 0x80000000 --pagesize 2048 --kernel_offset 0x0000
 TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilt/kernel
 
 TARGET_KMODULES := true
-# Disable memcpy opt (for audio libraries)
-TARGET_CPU_MEMCPY_OPT_DISABLE := true
 
-# Display
-USE_OPENGL_RENDERER := true
-BOARD_EGL_CFG := $(LOCAL_PATH)/configs/egl.cfg
-TARGET_SCREEN_HEIGHT := 1280
-TARGET_SCREEN_WIDTH := 720
-BOARD_EGL_WORKAROUND_BUG_10194508 := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun/file
 
-# Flags
-TARGET_GLOBAL_CFLAGS   += -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
-COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
-COMMON_GLOBAL_CFLAGS += -DDISABLE_HW_ID_MATCH_CHECK
-TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
-
-# LightHAL
-TARGET_PROVIDES_LIBLIGHT := true
-
-# Enable Minikin text layout engine (will be the default soon)
-USE_MINIKIN := true
-
-# Charger
-BOARD_CHARGER_SHOW_PERCENTAGE := true
-
-# Fonts
-EXTENDED_FONT_FOOTPRINT := true
-
-TARGET_SYSTEM_PROP := $(LOCAL_PATH)/system.prop
+# TWRP
+TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+TW_NO_REBOOT_BOOTLOADER := true
+TARGET_RECOVERY_FSTAB := device/mediatek/mt6580/recovery/root/etc/recovery.fstab
+TARGET_RECOVERY_DEVICE_DIRS += device/mediatek/mt6580
+TW_THEME := portrait_hdpi
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/mt_usb/musb-hdrc.0/gadget/lun%d/file
+TW_MAX_BRIGHTNESS := 255
+RECOVERY_SDCARD_ON_DATA := true
+TW_DEFAULT_EXTERNAL_STORAGE := true
+TW_CRYPTO_FS_TYPE := "ext4"
+TW_CRYPTO_REAL_BLKDEV := "/dev/block/mmcblk0p8"
+TW_CRYPTO_MNT_POINT := "/data"
+TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,noatime,discard,noauto_da_alloc,data=ordered"
+TW_NO_USB_STORAGE := true
+TW_EXCLUDE_SUPERSU := true
+TW_INCLUDE_FB2PNG := true
+TW_CUSTOM_CPU_TEMP_PATH := /sys/class/thermal/thermal_zone1/temp
+TW_EXTRA_LANGUAGES := true
+TW_BUILD_ZH_CN_SUPPORT := true
 
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
@@ -82,18 +106,26 @@ BOARD_HAVE_BLUETOOTH_MTK := true
 BOARD_BLUETOOTH_DOES_NOT_USE_RFKILL := true
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
 
-# GPS
-BOARD_GPS_LIBRARIES := true
-BOARD_MEDIATEK_USES_GPS := true
+# Sensors
+TARGET_NO_SENSOR_PERMISSION_CHECK := true
 
-# FM
-MTK_FM_SUPPORT := true
-MTK_FM_RX_SUPPORT := true
+# Offline charging
+BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/BOOT/BOOT/boot/boot_mode
+BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_CHARGER_DISABLE_INIT_BLANK := true
+BOARD_CHARGER_SHOW_PERCENTAGE := true
+BOARD_HAL_STATIC_LIBRARIES := libhealthd.mtk
+BOARD_HEALTHD_CUSTOM_CHARGER_RES := $(LOCAL_PATH)/charger/images
 
-# Consumerir
-MTK_IRTX_SUPPORT := true
+# EGL settings
+BOARD_EGL_CFG := $(LOCAL_PATH)/configs/egl.cfg
+USE_OPENGL_RENDERER := true
+BOARD_EGL_WORKAROUND_BUG_10194508 := true
+BOARD_EGL_NEEDS_HANDLE_VALUE := true
+BOARD_EGL_NEEDS_FNW := true
+TARGET_REQUIRES_SYNCHRONOUS_SETSURFACE := true
 
-# MTK_WLAN_SUPPORT
+# WIFI
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 BOARD_HOSTAPD_DRIVER := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_mt66xx
@@ -104,70 +136,50 @@ WIFI_DRIVER_FW_PATH_STA := STA
 WIFI_DRIVER_FW_PATH_AP := AP
 WIFI_DRIVER_FW_PATH_P2P := P2P
 
+# GPS
+BOARD_GPS_LIBRARIES := true
+BOARD_MEDIATEK_USES_GPS := true
+TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
+
+# Disable memcpy opt (for audio libraries)
+TARGET_CPU_MEMCPY_OPT_DISABLE := true
+
+# Enable Minikin text layout engine (will be the default soon)
+USE_MINIKIN := true
+
+# Selinux
+BOARD_SEPOLICY_DIRS += device/mediatek/mt6580/sepolicy
+
+# Hack for building without kernel sources
+$(shell mkdir -p $(OUT)/obj/KERNEL_OBJ/usr)
+
+# FMRadio
+MTK_FM_SUPPORT := true
+MTK_FM_RX_SUPPORT := true
+MTK_FM_CHIP := MT6627_FM
+FM_LIB_BUILD_MT6620 := yes
+FM_LIB_BUILD_MT6627 := yes
+FM_LIB_BUILD_MT6628 := yes
+FM_LIB_BUILD_MT6630 := yes
+
+# LightHAL
+TARGET_PROVIDES_LIBLIGHT := true
+
+# Fonts
+EXTENDED_FONT_FOOTPRINT := true
+
+TARGET_SYSTEM_PROP := $(LOCAL_PATH)/system.prop
+
+# Consumerir
+MTK_IRTX_SUPPORT := true
+
 # build old-style zip files (required for ota updater)
 BLOCK_BASED_OTA := false
 
-# make_ext4fs requires numbers in dec format
-TARGET_USERIMAGES_USE_EXT4 := true
-BOARD_BOOTIMAGE_PARTITION_SIZE := 20971520
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 20971520
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1610612736
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 13474725888
-BOARD_CACHEIMAGE_PARTITION_SIZE := 402653184
-BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_FLASH_BLOCK_SIZE := 131072
-
 BOARD_HAS_LARGE_FILESYSTEM := true
-TARGET_USERIMAGES_USE_EXT4 := true
 
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun/file
-
-BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_RECOVERY_SWIPE := true
 BOARD_SUPPRESS_EMMC_WIPE := true
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
-TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/recovery.fstab
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
-TARGET_USERIMAGES_USE_EXT4 := true
 
-# TWRP
-#TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
-#BOARD_HAS_NO_SELECT_BUTTON := true
-#TW_NO_REBOOT_BOOTLOADER := true
-#TARGET_RECOVERY_FSTAB := device/infinix/x510/recovery/root/etc/recovery.fstab
-#TARGET_RECOVERY_DEVICE_DIRS += deviсe/infinix/x510
-#TW_THEME := portrait_hdpi
-#RECOVERY_GRAPHICS_USE_LINELENGTH := true
-#TW_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness
-#TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/mt_usb/musb-hdrc.0/gadget/lun%d/file
-#TW_MAX_BRIGHTNESS := 255
-#RECOVERY_SDCARD_ON_DATA := true
-#TW_DEFAULT_EXTERNAL_STORAGE := true
-#TW_CRYPTO_FS_TYPE := "ext4"
-#TW_CRYPTO_REAL_BLKDEV := "/dev/block/mmcblk0p20"
-#TW_CRYPTO_MNT_POINT := "/data"
-#TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,noatime,discard,noauto_da_alloc,data=ordered"
-#TW_NO_USB_STORAGE := true
-#TW_EXCLUDE_SUPERSU := true
-#TW_INCLUDE_FB2PNG := true
-#TW_CUSTOM_CPU_TEMP_PATH := /sys/class/thermal/thermal_zone1/temp
-#TW_EXTRA_LANGUAGES := true
-#TW_BUILD_ZH_CN_SUPPORT := true
-
-# SELinux
-BOARD_SEPOLICY_DIRS := \
-       device/infinix/x510/sepolicy
-
-# Hack for build
-$(shell mkdir -p $(OUT)/obj/KERNEL_OBJ/usr)
-
-# RIL
-BOARD_RIL_CLASS := ../../../$(LOCAL_PATH)/ril
-
-BOARD_SEPOLICY_DIRS := $(LOCAL_PATH)/sepolicy
-
-TARGET_LDPRELOAD += libxlog.so:libmtk_symbols.so
-
-BOARD_SECCOMP_POLICY := $(LOCAL_PATH)/seccomp
-
-TARGET_TAP_TO_WAKE_NODE := /sys/devices/mx_tsp/gesture_control
