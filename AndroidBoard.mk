e19e5a505c6debaa31df940ff077a250d5ce5481
Ahmed Rahmy <ahmedrahmy842@gmail.com>
2017-04-16 15:53:20 +0200

    Bringing AOKP #2

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -5,12 +5,12 @@ include $(CLEAR_VARS)
 ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 
 # include the non-open-source counterpart to this file
--include vendor/mediatek/mt6580/AndroidBoardVendor.mk
+-include vendor/infinix/x510/AndroidBoardVendor.mk
 
 # copy keylayout overrides
 $(shell mkdir -p $(TARGET_OUT_KEYLAYOUT); \
-    cp -pf device/mediatek/mt6580/keylayout/Generic.kl $(TARGET_OUT_KEYLAYOUT))
+    cp -pf device/infinix/x510/keylayout/Generic.kl $(TARGET_OUT_KEYLAYOUT))
 
 # copy permissions overrides
 $(shell mkdir -p $(TARGET_OUT_ETC)/permissions; \
-    cp -pf device/mediatek/mt6580/configs/android.hardware.camera.xml $(TARGET_OUT_ETC)/permissions)
+    cp -pf device/infinix/x510/configs/android.hardware.camera.xml $(TARGET_OUT_ETC)/permissions)
