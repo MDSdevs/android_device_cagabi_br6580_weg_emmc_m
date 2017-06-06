bec468583bf2e7825f1f99c4e793772707f8f218
FeyoMx <elfeyo1980@gmail.com>
2017-06-06 15:09:15 -0500

    Update BoardConfig.mk
    Preparar TWRP

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -131,26 +131,27 @@ TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
 TARGET_USERIMAGES_USE_EXT4 := true
 
 # TWRP
-#TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
-#BOARD_HAS_NO_SELECT_BUTTON := true
-#TW_NO_REBOOT_BOOTLOADER := true
-#TARGET_RECOVERY_FSTAB := device/cagabi/br6580_weg_emmc_m/recovery/root/etc/recovery.fstab
-#TARGET_RECOVERY_DEVICE_DIRS += deviсe/cagabi/br6580_weg_emmc_m
-#TW_THEME := portrait_hdpi
-#RECOVERY_GRAPHICS_USE_LINELENGTH := true
-#TW_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness
-#TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/mt_usb/musb-hdrc.0/gadget/lun%d/file
-#TW_MAX_BRIGHTNESS := 255
-#RECOVERY_SDCARD_ON_DATA := true
-#TW_DEFAULT_EXTERNAL_STORAGE := true
-#TW_CRYPTO_FS_TYPE := "ext4"
-#TW_CRYPTO_REAL_BLKDEV := "/dev/block/mmcblk0p20"
-#TW_CRYPTO_MNT_POINT := "/data"
-#TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,noatime,discard,noauto_da_alloc,data=ordered"
-#TW_NO_USB_STORAGE := true
-#TW_EXCLUDE_SUPERSU := true
-#TW_INCLUDE_FB2PNG := true
-#TW_CUSTOM_CPU_TEMP_PATH := /sys/class/thermal/thermal_zone1/temp
+RECOVERY_VARIANT := twrp
+TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+TW_NO_REBOOT_BOOTLOADER := true
+TARGET_RECOVERY_FSTAB := device/cagabi/br6580_weg_emmc_m/recovery/root/etc/recovery.fstab
+TARGET_RECOVERY_DEVICE_DIRS += deviсe/cagabi/br6580_weg_emmc_m
+TW_THEME := portrait_hdpi
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/mt_usb/musb-hdrc.0/gadget/lun%d/file
+TW_MAX_BRIGHTNESS := 255
+RECOVERY_SDCARD_ON_DATA := true
+TW_DEFAULT_EXTERNAL_STORAGE := true
+TW_CRYPTO_FS_TYPE := "ext4"
+TW_CRYPTO_REAL_BLKDEV := "/dev/block/mmcblk0p20"
+TW_CRYPTO_MNT_POINT := "/data"
+TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,noatime,discard,noauto_da_alloc,data=ordered"
+TW_NO_USB_STORAGE := true
+TW_EXCLUDE_SUPERSU := true
+TW_INCLUDE_FB2PNG := true
+TW_CUSTOM_CPU_TEMP_PATH := /sys/class/thermal/thermal_zone1/temp
 #TW_EXTRA_LANGUAGES := true
 #TW_BUILD_ZH_CN_SUPPORT := true
 
