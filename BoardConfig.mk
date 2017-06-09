492c0494f0a3464798e88a55afb508f67726dbe9
FeyoMx <elfeyo1980@gmail.com>
2017-06-08 19:14:52 -0500

    Create BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,9 +41,10 @@ BOARD_USES_MTK_HARDWARE :=true
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32S1,32S1 androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_MKBOOTIMG_ARGS := --base 0x80000000 --pagesize 2048 --kernel_offset 0x00008000 --ramdisk_offset 0x04000000 --tags_offset 0x0e000000 --board R09
+BOARD_MKBOOTIMG_ARGS := --base 0x80000000 --pagesize 2048 --kernel_offset 0x00008000 --ramdisk_offset 0x04000000 --second_offset 0x80f00000 --tags_offset 0x0e000000 --board R09
 TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilt/kernel
-
+BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/bootimg.mk
+BOARD_CUSTOM_BOOTIMG := true
 TARGET_KMODULES := true
 # Disable memcpy opt (for audio libraries)
 TARGET_CPU_MEMCPY_OPT_DISABLE := true
